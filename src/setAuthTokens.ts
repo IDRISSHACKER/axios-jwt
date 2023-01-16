9e556827ad4b2868181980636f21d2b20ed96aa6
IDR6IX DUVAL <idrissleutch@cinaf.tv>
2023-01-16 12:38:56 +0100

    update config

--- src/setAuthTokens.ts
+++ src/setAuthTokens.ts
@@ -1,11 +1,10 @@
 import { StorageProxy } from './StorageProxy'
-// import { STORAGE_KEY } from './StorageKey'
+import { STORAGE_KEY } from './StorageKey'
 import { IAuthTokens } from './IAuthTokens'
 
 /**
  * Sets the access and refresh tokens
- * @param storage_key
  * @param {IAuthTokens} tokens - Access and Refresh tokens
  */
-export const setAuthTokens = (storage_key: string, tokens: IAuthTokens): void =>
-  StorageProxy.Storage?.set(storage_key, JSON.stringify(tokens))
+export const setAuthTokens = (tokens: IAuthTokens): void =>
+  StorageProxy.Storage?.set(STORAGE_KEY, JSON.stringify(tokens))

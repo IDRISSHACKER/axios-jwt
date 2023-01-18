794433af86737e47b3efd0d802c835cf44df8f21
IDR6IX DUVAL <idrissleutch@cinaf.tv>
2023-01-18 09:19:56 +0100

    modified param

--- src/setAuthTokens.ts
+++ src/setAuthTokens.ts
@@ -1,14 +1,11 @@
 import { StorageProxy } from './StorageProxy'
 import { IAuthTokens } from './IAuthTokens'
-import {StorageKey} from "./StorageKeyType";
-import {SetStorageKey} from "./StorageKeys";
+import {GetStorageKey} from "./StorageKeys";
 
 /**
  * Sets the access and refresh tokens
- * @param storage_key
  * @param {IAuthTokens} tokens - Access and Refresh tokens
  */
-export const setAuthTokens = (storage_key: StorageKey, tokens: IAuthTokens): void => {
-   SetStorageKey(storage_key)
-    StorageProxy.Storage?.set(storage_key, JSON.stringify(tokens))
+export const setAuthTokens = (tokens: IAuthTokens): void => {
+    StorageProxy.Storage?.set(GetStorageKey(), JSON.stringify(tokens))
 }
